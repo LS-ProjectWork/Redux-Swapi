a955ad032969d8b3565bac0611a91a5cbe9682eb
dpayton23 <darien.payton71@gmail.com>
2019-04-10 17:19:13 -0400

    created root reducer logic

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,14 +1,27 @@
 import /* we need our action types here*/ "../actions";
 const initialState = {
-  characters: []
-  // Array characters, Boolean fetching, null error.
+  characters: [],
+  isFetching: false,
+  error: ''
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
-    // Fill me in with the important reducers
-    // action types should be FETCHING, SUCCESS and FAILURE
-    // your switch statement should handle all of these cases.
-    default:
-      return state;
+    case FETCHING:
+      return {
+        ...state,
+        characters: [],
+        isFetching: true,
+        error: ''
+      }
+    case SUCCESS:
+      return {
+        ...state,
+        characters: action.payload.characters,
+        isFetching: false,
+        error: ''
+      }
+    case FAILURE:
+      default:
+        return state;
   }
 };
